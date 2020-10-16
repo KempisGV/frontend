3189a4f30a47590dc8d7041429f5fa5614b5abe0
Tomas Salvador <tsalvador@uees.edu.ec>
2020-10-16 12:20:41 -0500

    reemplazo de archivo

--- src/stores/UserStore.js
+++ src/stores/UserStore.js
@@ -1,9 +1,12 @@
 import { extendObservable } from 'mobx';
 
-class UserStore{
-    constructor(){
+/**
+ * UserStore
+ */
+class UserStore {
+    constructor() {
         extendObservable(this, {
-            
+
             loading: true,
             isLoggedIn: false,
             username: ''
